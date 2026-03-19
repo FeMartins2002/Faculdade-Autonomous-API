0ef00715851240952519b6ac971249cbe9ba4caa
Felipe <felipesj10@hotmail.com>
2026-03-19 09:02:29 -0300

    :wrench: Configura application.properties para trabalhar com H2

--- src/main/java/br/com/AutonomousAPI/AutonomousApiApplication.java
+++ src/main/java/br/com/AutonomousAPI/AutonomousApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AutonomousApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AutonomousApiApplication.class, args);
 	}
-
 }
