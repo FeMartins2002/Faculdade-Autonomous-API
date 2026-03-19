51b4bdabd6452bcf319914d059feb39b03b9ad16
Felipe <felipesj10@hotmail.com>
2026-03-19 08:50:12 -0300

    :tada: Commit Inicial

---
+++ src/main/java/br/com/AutonomousAPI/AutonomousApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.AutonomousAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonomousApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutonomousApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/AutonomousAPI/AutonomousApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.AutonomousAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutonomousApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
